4a649b7c1b1b1e125beb3c6519eb9cfd4b6ddf40
Tima Maslyuchenko <insside@gmail.com>
2014-08-01 11:12:59 +0300

    rewind active_record node only if subobject is not nil

--- lib/two_way_mapper/node/active_record.rb
+++ lib/two_way_mapper/node/active_record.rb
@@ -1,6 +1,10 @@
 module TwoWayMapper
   module Node
     class ActiveRecord < Object
+      def rewind_to?(obj, key)
+        super && obj.send(key)
+      end
+
       def create_node(obj, key)
         obj.send "build_#{key}"
       end
