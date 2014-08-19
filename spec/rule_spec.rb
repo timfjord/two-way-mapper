7acb2eea13b94e1010c4f5378e932764706577c0
Tima Maslyuchenko <insside@gmail.com>
2014-08-19 12:01:19 +0300

    add callbacks and default for left and right

--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -3,6 +3,7 @@ describe TwoWayMapper::Rule do
     let(:left_node) { TwoWayMapper::Node::Object.new 'key1' }
     let(:right_node) { TwoWayMapper::Node::Hash.new 'Kk.Key1' }
     let(:left_object) { OpenStruct.new }
+    let(:map) { { 'value' => 'VALUE' } }
 
     context 'without options' do
       let(:rule) { TwoWayMapper::Rule.new left_node, right_node }
@@ -29,7 +30,6 @@ describe TwoWayMapper::Rule do
     end
 
     context 'with map option' do
-      let(:map) { { 'value' => 'VALUE' } }
       let(:rule) { TwoWayMapper::Rule.new left_node, right_node, map: map, default: 'not found' }
 
       describe '#from_left_to_right' do
@@ -39,14 +39,6 @@ describe TwoWayMapper::Rule do
           rule.from_left_to_right left_object, right_object
 
           expect(right_object).to eql Kk: { Key1: 'VALUE' }
-        end
-
-        it 'should return default value if not found' do
-          left_object.key2 = 'value'
-          right_object = {}
-          rule.from_left_to_right left_object, right_object
-
-          expect(right_object).to eql Kk: { Key1: 'not found' }
         end
       end
 
@@ -58,13 +50,77 @@ describe TwoWayMapper::Rule do
 
           expect(left_object.key1).to eql 'value'
         end
+      end
+    end
 
+    context 'with default option' do
+      describe '#from_left_to_right' do
         it 'should return default value if not found' do
+          rule = TwoWayMapper::Rule.new left_node, right_node, map: map, default: 'not found'
+
+          left_object.key1 = 'value1'
+          right_object = {}
+          rule.from_left_to_right left_object, right_object
+
+          expect(right_object).to eql Kk: { Key1: 'not found' }
+        end
+
+        it 'should return use default_left if present and value not found' do
+          rule = TwoWayMapper::Rule.new left_node, right_node, map: map, default: 'not found', default_left: 'not found on left', default_right: 'not found on right'
+
+          left_object.key1 = 'value1'
+          right_object = {}
+          rule.from_left_to_right left_object, right_object
+
+          expect(right_object).to eql Kk: { Key1: 'not found on left' }
+        end
+      end
+
+      describe '#from_right_to_left' do
+        it 'should return default value if not found' do
+          rule = TwoWayMapper::Rule.new left_node, right_node, map: map, default: 'not found'
+
           left_object.key1 = nil
           right_object = { Kk: { Key1: 'VALUE1' } }
           rule.from_right_to_left left_object, right_object
 
           expect(left_object.key1).to eql 'not found'
+        end
+
+        it 'should return use default_right if present and value not found' do
+          rule = TwoWayMapper::Rule.new left_node, right_node, map: map, default: 'not found', default_left: 'not found on left', default_right: 'not found on right'
+
+          left_object.key1 = nil
+          right_object = { Kk: { Key1: 'VALUE1' } }
+          rule.from_right_to_left left_object, right_object
+
+          expect(left_object.key1).to eql 'not found on right'
+        end
+      end
+    end
+
+    describe 'with callback option' do
+      describe 'on_left_to_right' do
+        it 'should transform value if such options passed' do
+          rule = TwoWayMapper::Rule.new left_node, right_node, on_left_to_right: ->(v) { v.upcase }
+
+          left_object.key1 = 'value1'
+          right_object = {}
+          rule.from_left_to_right left_object, right_object
+
+          expect(right_object).to eql Kk: { Key1: 'VALUE1' }
+        end
+      end
+
+      describe 'on_right_to_left' do
+        it 'should transform value if such options passed' do
+          rule = TwoWayMapper::Rule.new left_node, right_node, on_right_to_left: ->(v) { v.downcase }
+
+          left_object.key1 = nil
+          right_object = { Kk: { Key1: 'VALUE1' } }
+          rule.from_right_to_left left_object, right_object
+
+          expect(left_object.key1).to eql 'value1'
         end
       end
     end
